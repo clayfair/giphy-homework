eb7b2261bec3934f28f15dcbc423fdd9f3d054c5
Clay Fairbanks <clayafairbanks@gmail.com>
2018-03-18 20:54:43 -0400

    updated

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -75,60 +75,9 @@ $(document).on("click", "button", function () {
                 heroDiv.append(heroImage);
 
                 //Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-                $("#animals").prepend(heroDiv);
+                $("#heroesPanel").prepend(heroDiv);
             }
         });
 
 });
 
-
-
-
-
-
-
-    //-- NOW THAT WEVE ACCESSED HER CLASS LETS USE IT FOR THE API TO APPEND IMAGES AND RATINGS -- 
-
-
-    // Constructing a queryURL using the animal name
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    //     hero + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    // // Performing an AJAX request with the queryURL
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // })
-    //     // After data comes back from the request
-    //     .then(function (response) {
-    //         console.log(queryURL);
-
-    //         console.log(response);
-    //         // storing the data from the AJAX request in the results variable
-    //         var results = response.data;
-
-    //         // Looping through each result item
-    //         for (var i = 0; i < results.length; i++) {
-
-    //             // Creating and storing a div tag
-    //             var heroDiv = $("<div>");
-
-    //             // Creating a paragraph tag with the result item's rating
-    //             var p = $("<p>").text("Rating: " + results[i].rating);
-
-    //             // Creating and storing an image tag
-    //             var heroImage = $("<img>");
-    //             // Setting the src attribute of the image to a property pulled off the result item
-    //             heroImage.attr("src", results[i].images.fixed_height.url);
-
-    //             // Appending the paragraph and image tag to the animalDiv
-    //             heroDiv.append(p);
-    //             heroDiv.append(heroImage);
-
-    //             // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-    //             $("#gifs-appear-here").prepend(heroDiv);
-    //         }
-    //     });
-
-
-//populate 10 GIFS and "rating: " + GIFrating 
